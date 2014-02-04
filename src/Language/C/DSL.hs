07804fa8d99f4d6992f48ad7585b9f08131ab44f
Danny Gratzer <danny.gratzer@gmail.com>
2014-02-04 12:00:29 -0600

    Exported Language.C from Language.C.DSL for convience

--- src/Language/C/DSL.hs
+++ src/Language/C/DSL.hs
@@ -2,7 +2,9 @@ module Language.C.DSL (
   module Language.C.DSL.StringLike,
   module Language.C.DSL.Exp,
   module Language.C.DSL.Stat,
-  module Language.C.DSL.Decl) where
+  module Language.C.DSL.Decl,
+  module Language.C) where
+import Language.C
 import Language.C.DSL.StringLike
 import Language.C.DSL.Exp
 import Language.C.DSL.Stat
