99530cc0368ee0bf7bc15bc7dbe3df5a80c9e266
Danny Gratzer <danny.gratzer@gmail.com>
2014-02-08 13:26:45 -0600

    Documentation for statements

--- src/Language/C/DSL/Stat.hs
+++ src/Language/C/DSL/Stat.hs
@@ -1,21 +1,30 @@
 {-# LANGUAGE FlexibleInstances #-}
 module Language.C.DSL.Stat where
 import Language.C
+
+-- | An if statement with no else clause
 cif :: CExpr -> CStat -> CStat
 cif exp stat = CIf exp stat Nothing undefNode
 
+-- | An if statement with an else clause
 cifElse :: CExpr -> CStat -> CStat -> CStat
 cifElse exp th el = CIf exp th (Just el) undefNode
 
+-- | A while loop, the 'CExpr' is the looping condition.
 while :: CExpr -> CStat -> CStat
 while exp stat = CWhile exp stat False undefNode
 
+-- | A for loop, an example use
+-- > for(int "x" .= 1, 1, PlusPlus `pre` "x")
+-- >   "printf"#[str "%d\n", "x"]
 for :: (CDecl, CExpr, CExpr) -> CStat -> CStat
 for (init, test, upd) block = CFor (Right init) (Just test) (Just upd) block undefNode
 
+-- | A for loop with no declarations.
 noDeclFor :: (CExpr, CExpr) -> CStat -> CStat
 noDeclFor (test, upd) block = CFor (Left Nothing) (Just test) (Just upd) block undefNode
 
+-- | A do while loop.
 doWhile :: CExpr -> CStat -> CStat
 doWhile exp stat = CWhile exp stat True undefNode
 
